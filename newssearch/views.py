5f148ca5817095949741d4c429a318110c921924
Teo <nicolae.teodor.pavel@gmail.com>
2018-12-17 13:31:04 +0200

    Adds pagination to search

--- newssearch/views.py
+++ newssearch/views.py
@@ -1,8 +1,10 @@
+import math
 from django.shortcuts import render
 from esutils.search import search as elastic_search
 
 PAGE_SIZE=30
-
+DELTA=2
+MIN_PAGE=1
 class Article:
     
     def __init__(self, title, description, content, date, author, url, real_degree, pa_fake, svc_fake):
@@ -18,14 +20,26 @@ class Article:
 
 def search(request):
     if 'query' in request.GET:
-        # TODO: paginare
         # TODO: highlights
         query = request.GET['query']
-        results = elastic_search(query, results_number=PAGE_SIZE, starting_position=0)
+        results = elastic_search(query, results_number=PAGE_SIZE, starting_position=(page - 1) * PAGE_SIZE)
         articles = [Article(**results["hits"][i]['_source']["doc"]) for i in range(min(PAGE_SIZE, results["total"]))]
         
+        # Pagination
+        page = int(request.GET['page']) if 'page' in request.GET else MIN_PAGE
+        total_pages = int(math.ceil(1.0 * results["total"] / PAGE_SIZE))
+        page = max(min(page, total_pages), MIN_PAGE)
+        
+        min_page_ = max(page - DELTA, MIN_PAGE)
+        max_page_ = min(page + DELTA, total_pages)
+        
+        min_page = max(page - DELTA - (DELTA + page - max_page_) , MIN_PAGE)
+        max_page = min(page + DELTA + abs(DELTA + min_page_ - page), total_pages)
+        pages = range(min_page, max_page + 1)
+        # End of Pagination
+
         # Used for debuging
         # articles = [Article("title", "description", "18-11-2018", "Teo", "https://google.com" , 60, True, False)]
-        return render(request, 'list.html', {'articles': articles, 'query': query })
+        return render(request, 'list.html', {'articles': articles, 'query': query, 'pages': pages, 'current_page': page, 'first_page': page == MIN_PAGE, 'last_page': page == total_pages })
     else:
         return render(request, 'list.html')
